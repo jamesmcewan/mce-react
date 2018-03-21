2cdbcc90daae9da8a45046ce44a99ed7534da890
James McEwan <james@mcewan.io>
2018-03-21 10:01:57 +0000

    testing things!

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { mount } from 'enzyme';
+import App from "./App";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("App", () => {
+  it("should render without crashing", () => {
+    const wrapper = mount(<App />);
+    expect(wrapper).toHaveLength(1);
+  });
 });
