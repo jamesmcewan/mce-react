177ce08b9fd1e75fde04573ae2d131e45307dc51
James McEwan <james@mcewan.io>
2018-03-21 14:45:49 +0000

    trying things!

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { render } from 'react-snapshot';
 import App from "./components/App/App";
 import registerServiceWorker from "./registerServiceWorker";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(render(<App />, document.getElementById("root")));
 registerServiceWorker();
