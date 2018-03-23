9cc85dec9f4692a9d6af8d8ab556e445b044a0af
James McEwan <james@mcewan.io>
2018-03-23 10:57:51 +0000

    more things!

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { render } from "react-snapshot";
 import App from "./components/App/App";
 import registerServiceWorker from "./registerServiceWorker";
 const data = require('./data.json');
 
-ReactDOM.render(render(<App {...data} />, document.getElementById("root")));
+ReactDOM.render(<App {...data} />, document.getElementById("root"));
 registerServiceWorker();
